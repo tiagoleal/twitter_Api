6a411c19ade6e683df370083eeeaef0ff5e8c1f1
Tiago Leal <tiago.sleal12@gmail.com>
2020-07-07 13:44:38 -0300

    generate controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      post 'user_token' => 'user_token#create'
+      post 'user_token', to: 'user_token#create'
+      resources :follows, only: %i[create destroy]
+      resources :likes, only: %i[create destroy]
+      resources :tweets, only: %i[index show create update destroy]
+      resources :users, only: %i[show create update destroy] do
+        member do
+          get 'following'
+          get 'followers'
+        end
+        get 'current', on: :collection
+      end
     end
   end
 end
